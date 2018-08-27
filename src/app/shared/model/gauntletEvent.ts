86bb0c698dd36b4e80bcfe5a0b88e75d5128f9b8
srcooney <steven.r.cooney@gmail.com>
2018-08-26 21:00:27 -0500

    Moves the forced All access time to when you get the event
    - the all access time in the event no longer matters

--- src/app/shared/model/gauntletEvent.ts
+++ src/app/shared/model/gauntletEvent.ts
@@ -21,6 +21,9 @@ export class GauntletEvent {
     }
 
     static fromJson({$key,title,description,imageURL,maxNumUsers,currNumUsers,eventStartTime,allAccessTime,eventCreator,eventCreatorKey}):GauntletEvent {
+        var moment = require('moment');
+        var forcedAllAccessTime = moment.utc("2018-09-03T12:00:00-05:00").format('MM/DD/YYYY HH:mm').toString();
+
         return new GauntletEvent(
             $key,
             title,
@@ -29,7 +32,7 @@ export class GauntletEvent {
             maxNumUsers,
             currNumUsers,
             new Date(eventStartTime),
-            new Date(allAccessTime),
+            new Date(forcedAllAccessTime),
             eventCreator,
             eventCreatorKey
             );
